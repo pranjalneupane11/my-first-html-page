49b8307a0111ac032feadf55eb2ca9d5fa4fd343
Pranjal Neupane <pranjalneupane01@gmail.com>
2026-05-19 22:06:15 -0400

    Add files via upload

---
+++ readme.md
@@ -0,0 +1,24 @@
+# Python Student Profile
+
+This is my first Python project.
+
+## About Project
+This program prints a student profile using Python.
+
+It includes:
+- Variables
+- Comments
+- Print statements
+- Arithmetic operations
+- f-strings
+
+## Technologies Used
+- Python
+
+## Author
+Pranjal Neupane
+
+## Future Improvements
+- Add user input
+- Add more profile details
+- Make program interactive
---
+++ student.py
@@ -0,0 +1,21 @@
+
+name= "Pranjal"
+Age= 21
+Country= "Nepal"
+University= "Youngstown State University"
+Major= "CS"
+Dream_Company= "Google"
+Favorite_Game = "FC 26"
+
+#finding age after 5 years
+#current age= 21
+print("---------Student Profile--------")
+future_age = Age+5
+print("My name is:", name)
+print("I am originally from:", Country)
+print("I am currently studying at:",University)
+print("My major is", Major)
+print("I aim to work at:", Dream_Company)
+print("In my spare time , I love to play:",Favorite_Game)
+print(f"2.My current age is : {Age}. my age after five years will be :{future_age}.")
+print("3.I will never stop learning python and succeed in my carrer.")
